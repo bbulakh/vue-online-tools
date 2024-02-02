3d9013f081da6c45751c971591dad3323c3f8741
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-03 02:32:25 +0300

    rename HelloWorld to Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-4xl">Cools</h1>
+  <div class="px-[2rem]">
+    <h1 class="text-6xl">Cools</h1>
     <h3>
       Collection of useful and fun tools that you can use for your purposes
     </h3>
