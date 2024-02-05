91b7bb5090bbf43baf6c48fffdda4f402aad0eeb
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-05 16:16:30 +0300

    add ButtonPrimary component

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  props: {
+    btnText: String,
+    bgColor: String,
+    textColor: String,
+  }
+}
+</script>
+
+<template>
+  <button :class="['w-full', 'py-3', 'rounded-md', bgColor, textColor]">
+    {{ btnText }}
+  </button>
+</template>
+
+<style scoped>
+button,
+a {
+  font-family: 'Koulen';
+}
+</style>
