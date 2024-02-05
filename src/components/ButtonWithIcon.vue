54676905cf2486f115bb155af1972b7953c217ee
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-05 17:36:11 +0300

    add ButtonWithIcon component

---
+++ src/components/ButtonWithIcon.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    bgColor: String,
+    textColor: String
+  }
+}
+</script>
+
+<template>
+  <button
+    :class="[
+      'w-full',
+      'flex',
+      'justify-center',
+      'items-center',
+      'gap-2',
+      'py-3',
+      'rounded-md',
+      bgColor,
+      textColor
+    ]"
+  >
+    <slot name="icon"></slot>
+    <slot><span></span></slot>
+  </button>
+</template>
+
+<style scoped>
+button,
+a {
+  font-family: 'Koulen';
+}
+</style>
