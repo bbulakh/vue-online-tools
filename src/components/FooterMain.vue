fdc7694203cf73e0d437dd0f4f0ce30bd8ebebaa
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-05 16:29:34 +0300

    add draft form to footer

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,16 +1,38 @@
+<script setup>
+  import ButtonPrimary from './ButtonPrimary.vue'
+</script>
+
 <template>
   <footer class="bg-[#242424] text-white">
-    <div class="p-[2rem]">
-        <img class="w-1/2" src="/src/assets/images/logo-inverted.svg" alt="Cools inverted logo">
-        <p class="text-white w-1/2">Cools is a bunch of useful and fun tools that you can use for your purposes</p>
-        
-        <div class="flex flex-col">
-            <h2>Feel free to send me a feedback</h2>
-            <textarea class="text-black p-2" name="" id="" cols="30" rows="5"></textarea>
-            <button>SEND MESSAGE</button>
-        </div>
-    </div>
+    <div class="p-[2rem] flex flex-col gap-4">
+      <div>
+        <img
+          class="w-1/2 mb-2"
+          src="/src/assets/images/logo-inverted.svg"
+          alt="Cools inverted logo"
+        />
+        <p class="text-white w-2/3">
+          Cools is a bunch of useful and fun tools that you can use for your purposes
+        </p>
+      </div>
 
+      <div class="flex flex-col">
+        <h2 class="text-3xl text-left mb-3">Feel free to send me your feedback</h2>
+        <form class="flex flex-col gap-3">
+          <input class="p-2" type="text" placeholder="Enter Your Name" />
+          <input class="p-2" type="text" placeholder="E-mail" />
+          <textarea
+            class="text-black p-2"
+            placeholder="Write your ideas here..."
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+          ></textarea>
+          <ButtonPrimary bgColor="bg-[#EA985D]" btnText="SEND FEEDBACK" textColor="text-white" />
+        </form>
+      </div>
+    </div>
 
     <div class="flex justify-center items-center text-black bg-[#A8EB53]">
       <h2>{{ new Date().getFullYear() }}, Bogdan Bulakh</h2>
