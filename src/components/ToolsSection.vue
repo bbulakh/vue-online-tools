922d7599078225157f7730e1ca1195eb331a1b6d
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-06 14:54:38 +0300

    add draft tools section

---
+++ src/components/ToolsSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="relative mt-10 bg-[#F4EEDB] p-[2rem] overflow-hidden">
+    <h2 class="text-7xl mb-3 text-[#242424]">TOOLS</h2>
+    <div></div>
+    <input
+      class="w-full z-60 p-2 rounded-md outline-none mb-5"
+      type="search"
+      placeholder="Search Tool..."
+    />
+  </section>
+
+  
+
+
+
+</template>
+
